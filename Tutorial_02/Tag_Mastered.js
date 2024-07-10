0f3290b53722c596328604a5dc5515a66fc59f08
abuhasan-Ohovro <abuhasanmondal24@gmail.com>
2024-07-10 12:55:41 +0600

    Day_01/07 Done

---
+++ Tutorial_02/Tag_Mastered.js
@@ -0,0 +1,57 @@
+// * Here is some the HTML Tag that I have Mastered .
+
+ /*   1.<h1> to <h6> :: Heading Tag :HTML offers six heading tags, from <h1> to <h6>, each displaying the heading in a different font size.
+
+      2. <p> & <br> :: HTML <p> tags are used to write paragraph statements on a webpage. They start with the <p> tag and end with </p>. The HTML <br> tag is used to insert a single line break and does not require a closing tag. In HTML, the break tag is written as <br>.
+
+      3. <hr> ::  The HTML <hr> tag is used to divide a page into sections by creating a horizontal line that spans from the left to the right side of the page. This is an empty tag and does not require a closing tag or any additional attributes.
+
+      4. <img src="" > :: The <img> tag is used to insert an image into a webpage. The source of the image is specified within the src attribute, like this: <img src=”source_of_image”>.
+
+      5. <pre></pre> :: The HTML <pre> tag is used for pre-formatted text. It keeps the original spaces and line breaks exactly as they are in the code. When you use <pre>, the text appears in a fixed-width font, preserving the formatting and layout just as it looks in the HTML code.
+
+      6.<em> & <strong> :: These tags denote the logical or semantic value of the text. They convey meaning without directly affecting the visual appearance.
+                               <em>: Emphasizes text (commonly styled as italic).
+                               <strong>: Indicates important content (often styled as bold).
+
+      7 . Physical Tags
+These tags directly impact the visual appearance of the text, altering font styles, sizes, and other visual properties.
+
+                         #TODO:  HTML Formatting Elements  :: 
+
+                               <b>      : Displays text in a bold format.
+                               <i>      : Italicizes text.
+                               <small>  : Renders text in a smaller font size.
+                               <ins>    : Highlights added or inserted text.
+                               <sub>    : Creates subscript text.
+                               <strong> : Emphasizes text with importance, often in bold.
+                               <mark>	: Accentuates text with a background highlight.
+                               <del>	: Strikes through text to signify deletion.
+                               <em>	: Adds emphasis to text, commonly styled as italic.
+                               <sup>	: Formats text as superscript.   */
+
+
+//?   The use of HTML Quotations in which <bdo> adjusts text directionality.<abbr> clarifies the abbreviation “GfG” with its full title.<address> displays contact information. These elements enhance content organization and readability.                               
+
+//?  The use of HTML Quotations in which <bdo> adjusts text directionality.<abbr> clarifies the abbreviation “GfG” with its full title.<address> displays contact information. These elements enhance content organization and readability.
+
+
+
+ 
+      
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
